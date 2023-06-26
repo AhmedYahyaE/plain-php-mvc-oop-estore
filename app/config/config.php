4544bfba67b84f1356e805d24e00579916955b8a
Ahmed Yahya <a.yahya1@yahoo.com>
2023-06-26 05:31:28 +0300

    Minor change

--- app/config/config.php
+++ app/config/config.php
@@ -15,7 +15,7 @@ define('JS', '/js/');
 // Database Credentials
 defined('DATABASE_HOST_NAME')       ? null : define ('DATABASE_HOST_NAME', 'localhost');
 defined('DATABASE_USER_NAME')       ? null : define ('DATABASE_USER_NAME', 'root');
-defined('DATABASE_PASSWORD')        ? null : define ('DATABASE_PASSWORD', '0000');
+defined('DATABASE_PASSWORD')        ? null : define ('DATABASE_PASSWORD', '');
 defined('DATABASE_DB_NAME')         ? null : define ('DATABASE_DB_NAME', 'storedb');
 defined('DATABASE_PORT_NUMBER')     ? null : define ('DATABASE_PORT_NUMBER', 3306);
 defined('DATABASE_CONN_DRIVER')     ? null : define ('DATABASE_CONN_DRIVER', 1);
